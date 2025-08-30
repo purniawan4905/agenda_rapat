1f1635b8295a6b3e7c5c53e8d0ea2bd6fba9e853
purniawan4905 <purniawan4905@gmail.com>
2025-08-30 15:23:34 +0700

    update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_BASE_URL; // const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://agendarapat-production.up.railway.app/api'; // const API_BASE_URL = 'http://localhost:5000/api';
 
 class ApiService {
   private token: string | null = null;
